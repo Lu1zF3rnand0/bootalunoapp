fcb6dba6971610db7f815f325fede32eb9c889f8
luizf <luizf@DESKTOP-6LH8OFP.mshome.net>
2021-07-31 15:29:28 -0300

    Teste 2

--- src/main/java/br/edu/infnet/bootalunoapp/service/IncricaoService.java
+++ src/main/java/br/edu/infnet/bootalunoapp/service/IncricaoService.java
@@ -24,7 +24,7 @@ public class IncricaoService {
 	public void inscrever(Aluno aluno, Turma turma) {
 		turma.getAlunos().add(aluno);
 		alunoRepo.save(aluno);
-		//turmaRepo.save(turma);
+		turmaRepo.save(turma);
 	}
 
 }
